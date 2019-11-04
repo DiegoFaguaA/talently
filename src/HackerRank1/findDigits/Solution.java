0a3ea87d10a49c5f5669bd6d133f23e2e88c80e4
Diego Fagua Alfonso <diego.fagua@gmail.com>
2019-11-04 18:30:34 -0500

    HackerRank1 3 exercises complete, bomberman 40% resolved

---
+++ src/HackerRank1/findDigits/Solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        int[] digits = String.valueOf(n).chars().map(Character::getNumericValue).toArray();
+        int count = 0;
+        for (int i = 0; i < digits.length; i++){
+            if (digits[i] != 0 && n%digits[i]==0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
